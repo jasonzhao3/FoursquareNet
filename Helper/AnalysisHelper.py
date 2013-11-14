5b21d9aa5514af79aea8ab3fedc406337771aba0
jason zhao <yzhao3@stanford.edu>
2013-11-14 06:36:07 -0800

    final commit before milestone

--- Helper/AnalysisHelper.py
+++ Helper/AnalysisHelper.py
@@ -21,12 +21,11 @@ def get_mle_alpha(point_list, x_min):
     est_alpha = 1 + len(point_list) / np.sum(ratio_list)
     return est_alpha
 
-
 def gen_powerlaw_p(x, alpha, x_min):
     return (alpha-1.0)/x_min * pow((x/x_min), -alpha)
 
 def get_powerlaw_y(point_list, alpha, x_min, scale):
-    return [gen_powerlaw_p(x, alpha, x_min)*scale for x in xrange(1, len(point_list))]
+    return [gen_powerlaw_p(x, alpha, x_min)*scale for x in xrange(x_min, len(point_list))]
 
 
 
