8f83e6f54c9ac7bdba3a01c65ab22c5325fd110b
jason zhao <yzhao3@stanford.edu>
2013-11-13 20:41:03 -0800

    meet DelNode bug

--- Helper/GraphHelper.py
+++ Helper/GraphHelper.py
@@ -7,7 +7,6 @@ def gen_degree_hist( graph ):
 	snap.GetDegCnt( graph, deg_cnt_v )
 	return deg_cnt_v
 
-
 # load graph 
 def load_graph(data_path, filename):
 	graph_in = snap.TFIn(os.path.join(data_path, filename))
@@ -19,4 +18,44 @@ def save_graph(graph, data_path, filename):
     graph_out = snap.TFOut(out_file)
     graph.Save(graph_out)
     graph_out.Flush()
+    print "successfully save a graph! hoho~"
+
+
+'''
+    Debug related functions
+    Print Graph information
+'''
+def print_node_attr_names(graph):
+    node_attr_v = snap.TStrV()
+    graph.AttrNameNI(0, node_attr_v)
+    for item in node_attr_v:
+        print item  
+
+def print_edge_attr_names(graph):
+    edge_attr_v = snap.TStrV()
+    graph.AttrNameEI(0, edge_attr_v)
+    for item in edge_attr_v:
+        print item
+
+def print_nids(graph):
+    nids = []
+    for node in graph.Nodes():
+        nids.append(node.GetId())
+    nids.sort()
+    print nids
+
+'''
+    Snapshot related functions
+'''
+# delete node based on its checkin starting timestamp
+def filter_node_sts(graph, ts):
+    for node in graph.Nodes():
+        nid = node.GetId()
+        print nid
+        sts = graph.GetIntAttrDatN(nid, 'sts')
+        #the node appears later than current timestamp
+        if sts > ts:
+            print sts, ts
+            graph.DelNode(nid)
+
 
--- snapshot_graph.py
+++ snapshot_graph.py
@@ -3,8 +3,8 @@ import Helper.GraphHelper as GH
 import Helper.TimeHelper as TH
 import Helper.VenueHelper as VH
 
-print TH.gen_ts_list('201201010000', '201301010000', 30)
-
+# file configuration
+# TODO: create a configuration module
 graph_path = '../DataSet/GraphData/'
 venue_path = '../DataSet/VenueData/'
 result_path = '../DataSet/Analysis/'
@@ -13,11 +13,37 @@ category_name = 'category_map.json'
 pcategory_name = 'pcategory_map.json'
 
 # load graph and corresponding category_list
+'''
+    Currently, the graph has node attribute:
+    - vid
+    - ckf
+    - sts
+    - ets
+    - lat
+    - lng
+    - category
+    - pcategor
+    
+    And edge attribute:
+    - freq
+    - duration
+'''
 venue_g = GH.load_graph(graph_path, graph_name)
 category_list = VH.get_category_list(venue_path, category_name)
-print category_list
 pcategory_list = VH.get_category_list(venue_path, pcategory_name)
+#GH.print_node_attr_names(venue_g)
+#GH.print_edge_attr_names(venue_g)
+#print category_list
+GH.print_nids(venue_g)
 
 
-edge_id = venue_g.GetEId(1,2)
-print VH.get_category(venue_g, category_list, 1)
+# create snapshop of the graph - node accurate, but edge aren't
+ts_list = TH.gen_ts_list('201201010000', '201301010000', 30)
+ts_list.reverse()
+#venue_g.DelNode(14)
+for ts in ts_list:
+    GH.filter_node_sts(venue_g, ts)
+    GH.save_graph(venue_g, graph_path, 'sf_venue_'+ts)
+
+
+
