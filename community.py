e12b9fbf0c0f89c51009f79e3d3b06f4a113bc67
Ningxia Zhang <thmmrs2298@gmail.com>
2013-11-13 21:12:43 -0800

    try to get venue data

--- community.py
+++ community.py
@@ -1,6 +1,6 @@
 import snap
 import os
-import numpy as np
+import json
 import Helper.GraphHelper as GH
 
 def to_PUNGraph(g):
@@ -18,15 +18,40 @@ def to_PUNGraph(g):
 	print "node: %d edge: %d" % (node_count, edge_count)
 	return un_g
 
+def load_json(src_file):
+    json_file = open(src_file)
+    json_dict = json.load(json_file)
+    json_file.close()
+    return json_dict
+
+def to_hash(venue_list):
+	venue_hash = dict()
+	for item in venue_list:
+		venue_hash[item['id']] = [item['category'], item['venuename'], item['lat'], item['lng']]
+	return venue_hash
+
 data_path = '../CS224W_Dataset/GraphData'
 filename = 'sf_trsn_graph_small'
 trsn_g = GH.load_graph(data_path, filename)
 un_trsn_g = to_PUNGraph(trsn_g)
 
 # try to use the SNAP library function to get the community structure
-# test_g = snap.GenRndGnm(snap.PUNGraph, 500, 1000)
-communities = snap.TCnComV()
-modularity = snap.CommunityGirvanNewman(un_trsn_g, communities)
-print "done community detection."
-for community in communities:
-	print [node for node in community]
+# communities = snap.TCnComV()
+# modularity = snap.CommunityCNM(un_trsn_g, communities)
+# print "Community detection complete, modularity score is", modularity
+# for community in communities:
+# 	print [node for node in community]
+communities = [[46, 2377, 3016], [260, 1442, 1988, 4536], [684, 974, 3056, 6266]]
+
+# fetch venue info and produce a csv for visualization
+data_path = '../CS224W_Dataset'
+src_file = os.path.join(data_path, 'venues-CA-new.json')
+venue_list = load_json(src_file)
+# transform the venue list to a hash
+venue_hash = to_hash(venue_list)
+
+for i in range(len(communities)):
+	for nodeID in communities[i]:
+		vid = trsn_g.GetStrAttrDatN(nodeID, 'vid')
+		print venue_hash[vid][1]
+
