8467977d63b34476546cb687fd438ce8bbbdfbd7
Ningxia Zhang <thmmrs2298@gmail.com>
2013-11-14 01:40:30 -0800

    check in analysis

--- analyze_ck_stat.py
+++ analyze_ck_stat.py
@@ -1,26 +1,60 @@
 '''
-    This script is used to analyze the property of checkin
-    -- when is the earliest and latest checkin
+    This script is used to analyze the the distribution of check-ins
 '''
 import snap
 import os, pickle
 import numpy as np
 import Helper.GraphHelper as GH
+import collections as cl
 import pylab as plt
 
 ''' Import Graph: graph is stored in binary form to save space, available in dropbox folder
     sf_trsn_graph_small: A small test graph with only a few venues in sf -- you can use this to test your script first
     sf_trsn_graph: up-to-date venue graph of sf
 '''
-data_path = '../DataSet/'
-result_path = '../DataSet/Analysis/'
-#trsn_in = open(os.path.join(data_path, 'sf_trsn'))
-time_in = open(os.path.join(data_path, 'sf_time'))
 
-#trsn_list = pickle.load(trsn_in)
-time_list = pickle.load(time_in)
-ts_list = [item[0] for item in time_list]
-ts_list.extend([item[1] for item in time_list])
-print min(ts_list), max(ts_list)
+def counter_to_arrays(c):
+	values = []
+	frequencies = []
+	for n in c:
+		values.append(n)
+		frequencies.append(c[n])
+	return [values, frequencies]
 
+data_path = '../CS224W_Dataset/GraphData'
+result_path = '../CS224W_Dataset/Analysis/'
+
+graph = GH.load_graph(data_path, 'sf_venue_graph')
+occurrences = cl.Counter()
+
+for node in graph.Nodes():
+	ckn = graph.GetIntAttrDatN(node.GetId(), 'ckn')
+	occurrences[ckn] += 1
+
+x, y = counter_to_arrays(occurrences)
+
+plt.figure()
+plt.xscale('log')
+plt.yscale('log')
+plt.scatter(x, y, color='crimson')
+plt.title('check-in distribution of venues')
+plt.ylabel('number of venues')
+plt.xlabel('total check-ins')
+plt.savefig(os.path.join(result_path, 'ck_freq_dist.png'))
+
+occurrences = cl.Counter()
+for edge in graph.Edges():
+	trsn_cnt = graph.GetIntAttrDatE(edge.GetId(), 'trsn_cnt')
+	occurrences[trsn_cnt] += 1
+
+x, y = counter_to_arrays(occurrences)
+
+plt.figure()
+plt.xscale('log')
+plt.yscale('log')
+plt.scatter(x, y, color='crimson')
+plt.title('transition counts distribution')
+plt.ylabel('number of edges')
+plt.xlabel('transition counts')
+plt.savefig(os.path.join(result_path, 'trsn_counts_dist.png'))
 
