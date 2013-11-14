f2bc8cb6ba53e769251f316d26eee6446dc15891
jason zhao <yzhao3@stanford.edu>
2013-11-13 20:53:39 -0800

    clean up GraphHelper.py

--- build_graph.py
+++ build_graph.py
@@ -8,50 +8,6 @@ import snap
 import os, pickle
 import Helper.GraphHelper as GH
 
-def add_time_stamp(graph, nid, time_stamp):
-    #the type bug was weird, has trapped me for a while
-    graph.AddIntAttrDatN(nid, int(time_stamp), 'sts')
-    graph.AddIntAttrDatN(nid, int(time_stamp), 'ets')
-    
-def update_time_stamp(graph, nid, time_stamp):  
-    time_stamp = int(time_stamp)
-    old_sts = graph.GetIntAttrDatN(nid, 'sts')
-    old_ets = graph.GetIntAttrDatN(nid, 'ets')
-    if time_stamp < old_sts:
-        graph.AddIntAttrDatN(nid, time_stamp, 'sts')
-    elif time_stamp > old_ets:
-        graph.AddIntAttrDatN(nid, time_stamp, 'ets')
-       
-def update_ckf(graph, nid):
-    ckf = trsn_g.GetIntAttrDatN(nid, 'ckf')
-    trsn_g.AddIntAttrDatN(nid, ckf+1, 'ckf')
-
-def add_node_attrs(graph, src_nid, dst_nid, time_range):
-    graph.AddIntAttrDatN(src_nid, 1, 'ckf')
-    graph.AddIntAttrDatN(dst_nid, 1, 'ckf')
-    add_time_stamp(graph, src_nid, time_range[0])
-    add_time_stamp(graph, dst_nid, time_range[1])
-
-def update_node_attrs(graph, src_nid, dst_nid, time_range):
-    update_ckf(graph, src_nid)
-    update_ckf(graph, dst_nid)
-    update_time_stamp(graph, src_nid, time_range[0])
-    update_time_stamp(graph, dst_nid, time_range[1])
-
-def add_edge_attrs(graph, src_nid, dst_nid, time_range):
-    edge_id = graph.AddEdge(src_nid, dst_nid)
-    graph.AddIntAttrDatE(edge_id, 1, 'freq')  
-    graph.AddFltAttrDatE(edge_id, int(time_range[1])-int(time_range[0]), 'duration')
-
-def update_edge_attrs(graph, src_nid, dst_nid, time_range):
-    edge_id = graph.GetEId(src_nid, dst_nid)
-    freq = graph.GetIntAttrDatE(edge_id, 'freq')
-    trsn_g.AddIntAttrDatE(edge_id, freq+1, 'freq')
-    duration = graph.GetFltAttrDatE(edge_id, 'duration')
-    new_duration = (duration + int(time_range[1]) - int(time_range[0])) / 2.0
-    graph.AddFltAttrDatE(edge_id, new_duration, 'duration')
-
-
 
 data_path = '../DataSet/'
 graph_path = '../DataSet/GraphData/'
@@ -89,12 +45,12 @@ for idx, trsn in enumerate(trsn_list):
     print src_nid, dst_nid
     #TODO: add timestamp filter
     if not trsn_g.IsEdge(src_nid, dst_nid):
-        add_edge_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
-        add_node_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
+        GH.add_edge_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
+        GH.add_node_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
         print "add a new edge, hoho~"
     else:
-        update_edge_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
-        update_node_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
+        GH.update_edge_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
+        GH.update_node_attrs(trsn_g, src_nid, dst_nid, time_list[idx])
         print "update node info, haha~"
     print idx, trsn
     print len(trsn_list)    
--- build_graph_score.py
+++ build_graph_score.py
@@ -21,27 +21,13 @@ result_path = '../DataSet/Analysis/'
 result_filename = 'sf_graph_with_attr'
 
 #TODO: make addnodeattr as a function in the GraphHelper
-# add attributes for graph nodes
-''' for each node in the graph, add two attributes
-    1. two float values: latitude, longitute
-    2. category
-    3. parent-category
-    '''
-def AddNodeAttr(graph, full_venue_dict, category_dict, pcategory_dict):
-    for NI in graph.Nodes():
-        vid = graph.GetStrAttrDatN(NI.GetId(), 'vid')
-        if vid in full_venue_dict:
-            graph.AddFltAttrDatN(NI.GetId(), float(full_venue_dict[vid]['lat']), 'lat')
-            graph.AddFltAttrDatN(NI.GetId(), float(full_venue_dict[vid]['lng']), 'lng')
-            graph.AddIntAttrDatN(NI.GetId(), category_dict[full_venue_dict[vid]['category']], 'category')
-            graph.AddIntAttrDatN(NI.GetId(), pcategory_dict[full_venue_dict[vid]['parentcategory']], 'pcategory')
 
 trsn_g = GH.load_graph(graph_data_path, graph_filename)
 full_venue_dict = VH.GetFullVenueDict(venue_graph_data_path, venue_filename)
 category_dict = VH.load_json(venue_graph_data_path, 'category_map.json')
 pcategory_dict = VH.load_json(venue_graph_data_path, 'pcategory_map.json')
 
-AddNodeAttr(trsn_g, full_venue_dict, category_dict, pcategory_dict)
+GH.add_category(trsn_g, full_venue_dict, category_dict, pcategory_dict)
 GH.save_graph(trsn_g, graph_data_path, 'sf_venue_graph')
 print 'successfully build venue_graph!'
 
